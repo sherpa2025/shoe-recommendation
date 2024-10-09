8646fd9b8e89c68c8ead6d633a064d5677f3f1da
Nima Sherpa <sherpa2025@gmail.com>
2024-10-08 18:39:15 -0600

    shoe logic complete

--- src/shoeRecommendationLogic.js
+++ src/shoeRecommendationLogic.js
@@ -3,24 +3,16 @@ function parseSize(size) {
     const wholePart = parseFloat(size.split(" ")[0]);
     return wholePart + 0.5;
   }
-
   // If there is no fractional part, just convert to float
   return parseFloat(size);
 }
 
-export const getStreetRecommendation = (
-  userChoosenBrand, // Recommended model
-  userUkSize, // User's UK size
-  comfortOption // Comfort option chosen
-) => {
+const getRecommendation = (userUkSize, adjustmentData, chosenBrand) => {
   // Normalize UK size
   const normalizedUkSize = parseSize(userUkSize);
 
   // Get the adjustment value for the recommended model based on the previous brand
-  const adjustment =
-    streetRecommendationData[comfortOption.toLowerCase()]?.[
-      userChoosenBrand.toLowerCase()
-    ] || "0";
+  const adjustment = adjustmentData[chosenBrand.toLowerCase()] || "0";
 
   // Convert the adjustment to a number
   const adjustmentValue = parseFloat(adjustment);
@@ -36,6 +28,52 @@ export const getStreetRecommendation = (
   return {
     recommendedSize: formattedSize,
   };
+};
+
+// Recommendation data based on previous climbing shoe
+const recommendationData = {
+  sportiva: {
+    oasi: "0",
+    "oasi lv": "0",
+    iati: "0",
+    mundaka: "0",
+    mastia: "-0.5",
+    tarifa: "0",
+    indalo: "0",
+    ra: "0.5",
+    "ra woman": "0.5",
+    inti: "0.5",
+    masai: "0.5",
+    tanta: "0",
+  },
+  scarpa: {
+    oasi: "-1",
+    "oasi lv": "-1",
+    iati: "-1",
+    mundaka: "-1",
+    mastia: "-1.5",
+    tarifa: "-1",
+    indalo: "-1",
+    ra: "-0.5",
+    "ra woman": "-0.5",
+    inti: "-0.5",
+    masai: "-0.5",
+    tanta: "-1",
+  },
+  tenaya: {
+    oasi: "0",
+    "oasi lv": "0",
+    iati: "0",
+    mundaka: "0",
+    mastia: "-0.5",
+    tarifa: "0",
+    indalo: "-0.5",
+    ra: "0.5",
+    "ra woman": "0.5",
+    inti: "0.5",
+    masai: "0.5",
+    tanta: "0",
+  },
 };
 
 // Street recommendation data
@@ -96,4 +134,29 @@ const streetRecommendationData = {
     masai: "-1",
     tanta: "-1",
   },
+};
+
+// Exported functions to get recommendations
+export const getShoeRecommendation = (
+  previousBrand,
+  userUkSize,
+  recommendedModel
+) => {
+  return getRecommendation(
+    userUkSize,
+    recommendationData[previousBrand.toLowerCase()] || {},
+    recommendedModel
+  );
+};
+
+export const getStreetRecommendation = (
+  userChoosenBrand,
+  userUkSize,
+  comfortOption
+) => {
+  return getRecommendation(
+    userUkSize,
+    streetRecommendationData[comfortOption.toLowerCase()] || {},
+    userChoosenBrand
+  );
 };
